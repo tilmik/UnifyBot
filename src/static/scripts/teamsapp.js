d522960c9c9d72c092de1d900b65bdd6b3742549
Sourjya Bhaumik <sobhaum@microsoft.com>
2024-10-03 10:33:38 +0530

    Fixed theme toggle

--- src/static/scripts/teamsapp.js
+++ src/static/scripts/teamsapp.js
@@ -6,13 +6,25 @@
     microsoftTeams.app.getContext().then(function (context) {
       if (context?.app?.host?.name) {
         updateHubState(context.app.host.name);
+        updateTheme(context.app.theme);
       }
     });
   });
 
   function updateHubState(hubName) {
     if (hubName) {
-      document.getElementById("hubState").innerHTML = "in " + hubName;
+      document.getElementById("hubState").innerHTML = "Your app is running in " + hubName;
+    }
+  }
+
+  function updateTheme(themeName) {
+    if (themeName) {
+      //document.getElementById("themeName").innerHTML = "Selected theme is " + themeName;
+      if (themeName == "dark") {
+        document.body.classList.toggle("dark-mode");
+      } else if (themeName == "contrast") {
+        document.body.classList.toggle("contrast-mode");
+      }
     }
   }
 })();
